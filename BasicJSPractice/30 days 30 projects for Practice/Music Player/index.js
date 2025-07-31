feb20ce7bac107cb12ceab3f8b9818e4557ef850
om <odbayar810@gmail.com>
2025-07-31 06:47:46 +0100

    music player audio, controls, currentTime

---
+++ BasicJSPractice/30 days 30 projects for Practice/Music Player/index.js
@@ -0,0 +1,48 @@
+console.log("welcome to music player");
+
+let progress = document.getElementById("progress");
+let song = document.getElementById("song");
+let controlIcon = document.getElementById("controlIcon");
+
+let isPause = false;
+let isPlay = false;
+song.onloadedmetadata = function () {
+  progress.max = song.duration;
+  progress.value = song.currentTime;
+};
+
+function playPause() {
+  if (isPlay) {
+    controlIcon.src = "icons/play-circle.svg";
+    isPause = true;
+    isPlay = false;
+    song.pause();
+    console.log("first " + isPause, isPlay);
+    return;
+  }
+  if (isPause) {
+    controlIcon.src = "icons/pause-circle.svg";
+    isPause = false;
+    isPlay = true;
+    console.log("second" + isPlay, isPause);
+    song.play();
+    return;
+  }
+  controlIcon.src = "icons/pause-circle.svg";
+  isPlay = true;
+  song.play();
+}
+
+if (song.play()) {
+  setInterval(() => {
+    progress.value = song.currentTime;
+  }, 500);
+}
+
+progress.onchange = function () {
+  song.play();
+  song.currentTime = progress.value;
+  controlIcon.src = "icons/pause-circle.svg";
+  isPause = false;
+  isPlay = true;
+};
